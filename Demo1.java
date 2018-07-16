a1bf3dc8a73b93624af940de6060b6ad9d0c4003
cbpantianya <41244142+cbpantianya@users.noreply.github.com>
2018-07-16 09:38:45 +0000

    1
    1

---
+++ Demo1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Demo1 {
+    	public static void main(String[] args) {
+    		System.out.println("欢迎使用!");
+    Scanner s1 = new Scanner(System.in);
+    int a1 = s1.nextInt();
+    if (a1 <= 18){
+    System.out.println("good");
+        }
+    if (a1 > 18) {
+    System.out.println("old");
+        }  
+        
+    	  }
+    }
